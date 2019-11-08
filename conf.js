cf7eb8a3a1ed4f860227da5cb4019042824dcbae
Charek Chen <charek@gmail.com>
2019-11-08 12:09:36 -0500

    Working test

---
+++ conf.js
@@ -0,0 +1,93 @@
+const DescribeFailureReporter = require('protractor-stop-describe-on-failure');
+var request = require('request-promise');
+
+var data_array=[]
+var specresult={}    
+
+var testsuite_name=''
+var describe_name=''
+
+var myReporter = {
+
+
+  jasmineStarted: function(suiteInfo) {
+  },
+
+  suiteStarted: async function(result) {
+    describe_name=result.description
+    try{
+        await browser.getProcessedConfig().then((config) => { 
+        testsuite_name=config.suite
+      }).catch(err =>  console.log(err))
+    }
+    catch (e) {
+      console.log(e)};
+  },
+
+  specStarted: function(result) {
+     
+  },
+
+  specDone: function(result) {
+    var specresult={}
+    specresult['eventType']='TestResult_1'
+    specresult['testsuite']=testsuite_name
+    specresult['describe']=describe_name
+    specresult['it']=result.description
+    specresult['result'] = result.status; 
+    data_array.push(specresult);
+
+  },
+
+  suiteDone: async function(result) {
+  },
+
+  jasmineDone:  async function(result) {
+  }
+}
+
+exports.config = {
+SELENIUM_PROMISE_MANAGER: false,
+  framework: 'jasmine',
+  suites: {mysuite1: ['spec1.js','spec2.js']},
+ 
+  directConnect: true,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+    args: ['--disable-gpu','--disable-dev-shm-usage','--no-sandbox']
+    }
+  },
+  onPrepare: function () {
+    
+    jasmine.getEnv().addReporter(DescribeFailureReporter(jasmine.getEnv()));
+    jasmine.getEnv().addReporter(myReporter);
+  },
+
+   onComplete: async () => {
+
+    console.log(JSON.stringify(data_array));
+
+    const options ={ 
+      uri: "https://insights-collector.newrelic.com/v1/accounts/1614879/events",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",  
+        "X-Insert-Key": "vuFsXye2t6scXqd-QhVHlXCIsA0twnst"
+      },
+      body: data_array,
+      json:true,
+      resolveWithFullResponse: true
+    }
+    try {
+        await request(options).then(function (response) {
+            console.log("Post succeeded with status %d", response.statusCode);}).
+                catch(function (err) {
+                    console.log("Post failed with error: "+err);});
+     }
+     catch (e) {
+      console.log(e)}; 
+
+   }
+
+}
---
+++ spec1.js
@@ -0,0 +1,42 @@
+
+
+describe('E2E testsuite1', function() {
+
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+
+
+  beforeEach( async () =>  {
+    await browser.get('http://juliemr.github.io/protractor-demo/');
+  });
+
+  it('should have a title', async () => {
+    expect(await browser.getTitle()).toEqual('Super Calculator');
+  });
+
+  it('should add one and two',  async () => {
+    await firstNumber.sendKeys(1);
+    await secondNumber.sendKeys(2);
+
+    await goButton.click();
+
+    expect(await latestResult.getText()).toEqual('3');
+  });
+
+  it('should add four and six',  async () => {
+    // Fill this in.
+    await firstNumber.sendKeys(4);
+    await secondNumber.sendKeys(6);
+    expect(await firstNumber.getText()).toEqual('4');
+    await goButton.click();
+
+    expect(await latestResult.getText()).toEqual('10');
+  });
+
+  it('should read the value from an input',  async () => {
+    await firstNumber.sendKeys(1);
+    expect(await firstNumber.getAttribute('value')).toEqual('1');
+  });
+});
